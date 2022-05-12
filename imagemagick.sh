528de6e3d6fb645165bd1f2c5f985190e831e3ed
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-12 15:33:53 -0400

    updating intro

--- imagemagick.sh
+++ imagemagick.sh
@@ -13,9 +13,19 @@ LOGO=f5-logo-white.png
 BACKGROUND="1920x1080-openslide.png"
 #BACKGROUND="corporate-template.png"
 
-convert ${BACKGROUND} -verbose -strip -resize 1920x1080 -quality 100 -density 72x72 -units pixelsperinch tmp.png
 
-convert -font Arial-Bold -pointsize 96 -fill ${INTROTITLEFONTCOLOR} -annotate +72+445 "${title}" tmp.png title.png
+#convert ${BACKGROUND} -verbose -strip -resize 1920x1080 -quality 100 -density 72x72 -units pixelsperinch tmp.png
+
+convert -size 1920x1080 xc:'#05335C' tmp.png
+convert tmp.png -fill '#F2B819' -stroke '#F2B819' -draw "rectangle 0,1065 1920,1080" yellow.png
+convert yellow.png -fill '#DB0021' -stroke '#DB0021' -draw "rectangle 770,1065 1920,1080" yellow-red.png
+convert yellow-red.png -fill '#EE593B' -stroke '#EE593B' -draw "rectangle 937,1065 1920,1080" yellow-red-orange.png
+convert yellow-red-orange.png -fill '#1C89C9' -stroke '#1C89C9' -draw "rectangle 1265,1065 1920,1080" yellow-red-orange-cyan.png
+convert yellow-red-orange-cyan.png -fill '#0E487A' -stroke '#0E487A' -draw "rectangle 1593,1065 1920,1080" yellow-red-orange-cyan-blue.png
+
+convert yellow-red-orange-cyan-blue.png ${BACKGROUND} -composite hero-yellow-red-orange-cyan-blue.png
+
+convert -font Arial-Bold -pointsize 96 -fill ${INTROTITLEFONTCOLOR} -annotate +72+445 "${title}" hero-yellow-red-orange-cyan-blue.png title.png
 convert -font Arial-Bold -pointsize 96 -fill ${INTROCAPTIONFONTCOLOR} -annotate +72+543 "${caption}" title.png title-caption.png
 convert -font Arial-Bold -pointsize 31 -fill ${INTROCAPTIONFONTCOLOR} -annotate +72+660 "${presenter}" title-caption.png title-caption-presenter.png
 convert -font Arial-Bold -pointsize 31 -fill ${INTROCAPTIONFONTCOLOR} -annotate +72+728 "${position}" title-caption-presenter.png title-caption-presenter-position.png
@@ -24,4 +34,4 @@ convert -font Arial-Bold -pointsize 31 -fill ${INTROCAPTIONFONTCOLOR} -annotate 
 #convert title-caption-presenter-position-date.png ${LOGO} -gravity northeast -geometry 130x130+110+110 -composite intro.png
 convert title-caption-presenter-position-date.png ${LOGO} -gravity northwest -geometry 95x95+75+75 -composite intro.png
 
-rm title-caption.png title.png tmp.png title-caption-presenter.png title-caption-presenter-position.png title-caption-presenter-position-date.png
+rm yellow.png yellow-red.png yellow-red-orange.png yellow-red-orange-cyan.png yellow-red-orange-cyan-blue.png hero-yellow-red-orange-cyan-blue.png title-caption.png title.png tmp.png title-caption-presenter.png title-caption-presenter-position.png title-caption-presenter-position-date.png
