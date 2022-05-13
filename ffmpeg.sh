a5e85ffc97d5f12bc9d40f4bb84f397223025cc8
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-13 11:40:34 -0400

    updating intro

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -6,14 +6,7 @@ ffmpeg -y -framerate .5 -i outro.png -c:v libx264 -pix_fmt yuv420p -r 29.01 outr
 
 #ffmpeg -y -f concat -safe 0 -i videos.txt -filter_complex "gltransition=duration=4:offset=1.5" -pix_fmt yuv420p -c copy output.mov
 
-ffmpeg-concat -t circleopen -d 750 -o output2.mov intro.mov outro.mov
+#ffmpeg-concat -t circleopen -d 750 -o output2.mov intro.mov outro.mov
 
-ffmpeg \
--pix_fmt yuv420p \
--i intro.mov \
--i outro.mov \
--filter_complex xfade=transition=fade:\
-duration=1:\
-offset=0 \
-outputVideo.mov
+ffmpeg -y -i intro.mov -i outro.mov -filter_complex xfade=transition=fade:duration=1:offset=1 -pix_fmt yuv420p outputVideo.mov
 
