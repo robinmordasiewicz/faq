5aad36db962c3764363544e4b30d27045194aa26
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-13 11:33:23 -0400

    updating intro

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -8,3 +8,12 @@ ffmpeg -y -framerate .5 -i outro.png -c:v libx264 -pix_fmt yuv420p -r 29.01 outr
 
 ffmpeg-concat -t circleopen -d 750 -o output2.mov intro.mov outro.mov
 
+ffmpeg \
+-pix_fmt yuv420p \
+-i intro.mov \
+-i outro.mov \
+-filter_complex xfade=transition=fade:\
+duration=1:\
+offset=0 \
+outputVideo.mov
+
