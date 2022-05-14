cd0cda0468048e06fd72713228d5adb34ade9fbc
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-14 14:02:36 -0400

    updating intro

--- mlt.sh
+++ mlt.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #
+
+melt intro.mlt -consumer avformat:intro.mp4 f=mp4 s=1920x1080 pix_fmt=yuv420p r=29.97
+
 #melt intro.mlt -consumer avformat:output.mov f=mov acodec=pcm_s16le vcodec=libxvid s=1920x1080 b=6000k pix_fmt=uyvy422 vtag=yuvs acodec=pcm_s16le
 
-melt intro.mlt -consumer avformat:output-mlt.mov f=mov acodec=copy vcodec=prores_ks vendor="apl0" s=1920x1080 pix_fmt=yuv420p r=29.97 s=1920x1080
+#melt intro.mlt -consumer avformat:output-mlt.mov f=mov acodec=copy vcodec=prores_ks vendor="apl0" s=1920x1080 pix_fmt=yuv420p r=29.97 s=1920x1080
+#melt intro.mlt -consumer avformat:output-mlt.mp4 f=mov acodec=copy vcodec=prores_ks vendor="apl0" s=1920x1080 pix_fmt=yuv420p r=29.97 s=1920x1080
 
 # -c:v prores_ks -profile:v 2 -vendor apl0 -bits_per_mb 8000 -pix_fmt yuv422p10le -r 29.97 -s 1920x1080 -video_track_timescale 30000
