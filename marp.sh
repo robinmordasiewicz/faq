45bfbdd656cd011223316b8225f3e347b1d224d5
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-18 12:22:14 -0400

    updating intro

--- marp.sh
+++ marp.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 #
 
-# docker run --rm --init -v $PWD:/home/marp/app/ -e LANG=$LANG marpteam/marp-cli intro.md --pptx
-#which marp
-#find / | grep marp
-#marp-cli intro.md --pptx
-id
-ls -al
-
 /home/marp/.cli/docker-entrypoint intro.md --pptx
