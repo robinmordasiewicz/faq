b049397e42c51042c163b58f59d04feff86f159a
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-21 21:12:13 -0400

    updating intro

--- marp.sh
+++ marp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
 
-#if test -f "/home/marp/.cli/docker-entrypoint"; then
-#    /home/marp/.cli/docker-entrypoint intro.md --theme marp-theme.css --allow-local-files --pptx
-#else
+if test -f "/home/marp/.cli/docker-entrypoint"; then
+    /home/marp/.cli/docker-entrypoint intro.md --theme marp-theme.css --allow-local-files --pptx
+else
     marp intro.md --theme marp-theme.css --allow-local-files --pptx
-#fi
+fi
 
