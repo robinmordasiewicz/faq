f81a14ab0ded4a72bd4b876aaf17073f10430bcd
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-23 19:15:51 -0400

    updating intro

--- mermaid-cli.sh
+++ mermaid-cli.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 #
 
+if [ -e intro.png ];then
+  rm intro.png
+fi
+
 /usr/local/bin/mmdc -p /puppeteer-config.json -i intro.mmd -o intro.png -C mermaid.css -c mermaid-config.json -t neutral
 
 #-i intro.mmd -o intro.png -C mermaid.css -c mermaid-config.json -t neutral
