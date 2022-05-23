8aa4c743cdfea048217bbd181359abdb04a52184
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-23 00:00:51 -0400

    updating intro

--- intro-diagram.py
+++ intro-diagram.py
@@ -30,7 +30,7 @@ edge_attr = {
 with Diagram(name="F5 Cloud Native Solutions", show=False, direction="LR", filename="SPK-diagram", outformat="png", graph_attr=graph_attr,node_attr=node_attr,edge_attr=edge_attr):
 
     subscribers = Mobile("Subscribers")
-    with Cluster("BIG-IP NEXT",graph_attr=graph_attr,node_attr=node_attr,edge_attr=edge_attr):
+    with Cluster("BIG-IP NEXT",graph_attr=graph_attr,edge_attr=edge_attr):
         spk = Nginx("Service Proxy")
         subscribers - spk
     with Cluster("Kubernetes"):
