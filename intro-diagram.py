e124a82347d12a8979e1a9de7edd3117798b1f95
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-22 23:44:48 -0400

    updating intro

--- intro-diagram.py
+++ intro-diagram.py
@@ -22,8 +22,12 @@ node_attr = {
     "fontsize": "18",
     "bgcolor": "white"
 }
+edge_attr = {
+    "fontsize": "18",
+    "bgcolor": "white"
+}
 
-with Diagram(name="F5 Cloud Native Solutions", show=False, direction="LR", filename="SPK-diagram", outformat="png", graph_attr=graph_attr,node_attr=node_attr):
+with Diagram(name="F5 Cloud Native Solutions", show=False, direction="LR", filename="SPK-diagram", outformat="png", graph_attr=graph_attr,node_attr=node_attr,edge_attr=edge_attr):
 
     subscribers = Mobile("Subscribers")
     with Cluster("BIG-IP NEXT"):
