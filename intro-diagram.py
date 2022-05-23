9f21b8640e04ccfd9a207395be10cfadc9b8a667
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-23 19:06:53 -0400

    updating intro

--- intro-diagram.py
+++ intro-diagram.py
@@ -1,15 +1,4 @@
 from diagrams import Cluster, Diagram, Edge
-from diagrams.onprem.analytics import Spark
-from diagrams.onprem.compute import Server
-from diagrams.onprem.database import PostgreSQL
-from diagrams.onprem.inmemory import Redis
-from diagrams.onprem.aggregator import Fluentd
-from diagrams.onprem.monitoring import Grafana, Prometheus
-from diagrams.onprem.network import Nginx
-from diagrams.onprem.queue import Kafka
-from diagrams.k8s.compute import Pod
-from diagrams.k8s.network import Ing
-from diagrams.k8s.network import SVC
 from diagrams.k8s.compute import RS
 from diagrams.onprem.network import Istio
 from diagrams.generic.device import Mobile
