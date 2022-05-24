2ed8b1cec01c76ff65d84f75c4f2fcb3386135d7
robin@mordasiewicz.com <robin@mordasiewicz.com>
2022-05-23 22:34:00 -0400

    updating intro

--- intro-diagram.py
+++ intro-diagram.py
@@ -31,7 +31,7 @@ node_attr = {
 with Diagram(name="F5 - Cloud Native Solutions", show=False, direction="LR", filename="diagram1", outformat="png", graph_attr=global_attr,node_attr=node_attr):
 
     subscribers = Mobile("Subscribers")
-    Edge(color="darkorange",style="dashed",label="SIP HTTP2 Diamter")
+    subscribers - Edge(color="darkorange",style="dashed",label="SIP HTTP2 Diamter") - spk
     with Cluster("BIG-IP NEXT",graph_attr=spk_attr):
         spk = Custom("Service Proxy", "f5-logo-white.png")
         subscribers - spk
